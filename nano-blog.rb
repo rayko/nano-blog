939bae175d41d48c947b376d00f40d833923af79
Rayko <rayko.drg@gmail.com>
2023-08-15 22:39:25 -0300

    Setting content type and returning 204 on POST

--- nano-blog.rb
+++ nano-blog.rb
@@ -1,17 +1,24 @@
 require 'sinatra'
 
+settings.default_content_type = 'application/json'
+
+# TODO Return index.html
 get '/' do
   'Boot complete'
 end
 
+# TODO Prevent overloading (cache? rate limit?)
 get '/feed' do
   NBLogger.new.tail(20)
 end
 
+# TODO Secure this endpoint
 post '/feed' do
   message = params[:message]
   severity = params[:severity] || 'info'
   progname = params[:progname]
   level = NBLogger::SEVERITY_MAP.fetch(severity, Logger::INFO)
   log_thing = NBLogger.new.post(message, level, progname)
+
+  204
 end
