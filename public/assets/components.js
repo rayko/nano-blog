631c56095006d9d7583527eedda2719e2def5d57
Rayko <rayko.drg@gmail.com>
2023-09-07 20:26:29 -0300

    Refactoring components.js for convinience

--- public/assets/components.js
+++ public/assets/components.js
@@ -1,47 +1,49 @@
-function componentsElement() {
-  return document.getElementById('component-records');
-}
+window.components = {
+  element: function() {
+    return document.getElementById('component-records');
+  },
 
-function refreshComponents() {
-  element = 
-  path = componentsElement().getAttribute('data-path');
-  apiGET(path, renderComponents, componentsRequestError)
-}
+  refresh: function() {
+    path = components.element().getAttribute('data-path');
+    apiGET(path, components.load, components.requestError)
+  },
 
-function componentsRequestError(status) {
-  alert(status);
-}
+  requestError: function(status) {
+    alert(status);
+  },
 
-function renderComponents(data) {
-  componentsElement().innerHTML = '';
-  for (let i = 0; i < data.length; i++){
-    item = buildComponentItem(data[i]);
-    componentsElement().appendChild(item);
+  load: function(data) {
+    components.element().innerHTML = '';
+    for (let i = 0; i < data.length; i++){
+      item = components.buildItem(data[i]);
+      components.element().appendChild(item);
+    }
+  },
+
+  buildItem: function(record) {
+    container = document.createElement('div');
+    container.className = 'record-item';
+    container.setAttribute('data-id', record.id);
+    container.setAttribute('data-name', record.name);
+    
+    span = document.createElement('span');
+    span.innerHTML = record.id + ' - ' + record.name;
+    container.appendChild(span);
+
+    removeLink = document.createElement('a');
+    removeLink.className = 'action-link delete';
+    removeLink.setAttribute('href', '#');
+    removeLink.innerHTML = 'Remove';
+    removeLink.addEventListener('click', components.removeAction);
+    container.appendChild(removeLink);
+
+    return container;
+  },
+
+  removeAction: function(event) {
+    this.parentElement.remove();
+    alert('Removed');
+    event.preventDefault()
   }
 }
 
-function buildComponentItem(record){
-  container = document.createElement('div');
-  container.className = 'record-item';
-  container.setAttribute('data-id', record.id);
-  container.setAttribute('data-name', record.name);
-  
-  span = document.createElement('span');
-  span.innerHTML = record.id + ' - ' + record.name;
-  container.appendChild(span);
-
-  removeLink = document.createElement('a');
-  removeLink.className = 'action-link delete';
-  removeLink.setAttribute('href', '#');
-  removeLink.innerHTML = 'Remove';
-  removeLink.addEventListener('click', removeComponentAction);
-  container.appendChild(removeLink);
-
-  return container;
-}
-
-function removeComponentAction(event) {
-  this.parentElement.remove();
-  alert('Removed');
-  event.preventDefault()
-}
