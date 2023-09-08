9bea5f1f7dbfc0b752b7f35f99be0c515b409246
Rayko <rayko.drg@gmail.com>
2023-09-08 01:34:13 -0300

    Adding some flash message thingy and fade animations

--- public/assets/control.js
+++ public/assets/control.js
@@ -35,3 +35,37 @@ function inputAutoSize(event) {
 function afterLogEntryPost() {
   logEntries.refresh();
 }
+
+function flashMessage(message, messageType) {
+  container = document.getElementById('flash-container');
+  box = document.createElement('div');
+  box.className = messageType;
+  box.innerHTML = message;
+  container.appendChild(box);
+  box.style.opacity = 0;
+  fadeIn(box);
+}
+
+function fadeOut(element) {
+  opacity = 1.0;
+  interval = setInterval(function() {
+    opacity -= 0.15;
+    element.style.opacity = opacity;
+    if (opacity <= 0) {
+      clearInterval(interval);
+      element.remove();
+    }
+  }, 50);
+}
+
+function fadeIn(element) {
+  opacity = 0.0;
+  interval = setInterval(function() {
+    opacity += 0.15;
+    element.style.opacity = opacity;
+    if (opacity > 1) {
+      clearInterval(interval);
+      setTimeout(function() { fadeOut(element) }, 3000);
+    }
+  }, 50);
+}
